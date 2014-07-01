e9c7adba836a407e2e630b2c05e1334375ae74cc
David Barnes <david.g.barnes@monash.edu>
2014-07-01 16:12:15 +1000

    Update pdfsurfer.c
    add license GPL

--- pdfsurfer.c
+++ pdfsurfer.c
@@ -1,11 +1,25 @@
 /* pdfsurfer.c: render surfaces in OBJ format to 3-d PDF
  *
+ * Monash Web Surfer: web surface visualisation utility
+ * (c) David G. Barnes, Monash University 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
  * usage: echo /s2null | pdfsurfer [path/to/]model.tok
  * (then) wsxpdf.pl [path/to/]model.tok
  * (then) pdflatex wsxpdf; # rpt 3 times
  * (then) open wsxpdf.pdf
- *
- * David Barnes, Oct 2012
  *
  */
 
